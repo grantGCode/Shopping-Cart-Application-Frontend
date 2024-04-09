e18faa4f9e5829bb27e74613d6ab650161ac39f8
Grant Bentley <gbentley0618@comcast.net>
2024-04-09 11:07:34 -0500

    remove bootstrap from Store.jsx page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,6 +1,6 @@
 import Navbar from '../components/NavBar.js'
 import { products} from '../productStore.js'
-import {Row, Col, Form, Button,} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 import ProductCard from '../components/ProductCard.js'
 import { useShoppingCartContext } from '../CartContext.js'
  
@@ -10,31 +10,25 @@ function Store() {
   const {purgeShoppingCart} = useShoppingCartContext();
 
   return (
-    <div>
+    <>
       <Navbar />
-            <Row xs={1} md={3}>
-            {products.map((product) => (
-          <Col key={product.id} className="justify-content-center">
-            <ProductCard
-              className="border border-primary"
-              product={product} // Passing individual product as prop
-            />
-          </Col>
-        ))}
-            </Row>
-        <Row className="mt-4">
-          <Col className="mx-4">
-            <Form>
-                <Button
-                  size='lg'
-                  variant="danger" 
-                  onClick={purgeShoppingCart}
-                >Remove All From Cart
-                </Button>
-            </Form>
-          </Col>
-        </Row>
-    </div>
+        <div>
+          {products.map((product) => (
+            <div key={product.id}>
+              <ProductCard
+                product={product} // Passing individual product as prop
+              />
+            </div>
+          ))};
+        </div>
+          <Button
+            size='lg'
+            variant="danger" 
+            onClick={purgeShoppingCart}
+          >
+            Remove All From Cart
+          </Button>
+    </>
   )
 }
 
