2088ebb8c582d85ac3da87d183397da4af82416d
Grant Bentley <gbentley0618@comcast.net>
2024-04-23 17:28:06 -0500

    formated item card to new format

--- src/components/item-card/ItemCard.js
+++ src/components/item-card/ItemCard.js
@@ -1,34 +1,26 @@
 import {useContext} from "react"
 import { ShoppingCartContext } from '../../CartContext'
 import { getProductData } from '../../productStore'
-import { 
-    Form, 
-    Card,
-    Info, 
-    Title, 
-    Quantity, 
-    RemoveButton 
-} from "./ItemCard.styled";
+import { Form, } from "./ItemCard.styled";
 
 function ItemCard({item}) {
     const cartContext = useContext(ShoppingCartContext);
 
     const prodRef = getProductData(item.id)
 
-
     return(
         <Form>
-            <Card>
-                <Info>
-                    <Title>{prodRef.title}</Title>
-                    <Quantity>{`x${item.quantity}`}</Quantity>
-                </Info>
-                <RemoveButton
+            <div className="card">
+                <div className="info">
+                    <h3 className="title">{prodRef.title}</h3>
+                    <h5 className="quantity">{`x${item.quantity}`}</h5>
+                </div>
+                <button className="remove-button"
                     onClick={() => {cartContext.removeOneItem(item)}}
                 >
                     - Remove From Cart
-                </RemoveButton>
-            </Card>
+                </button>
+            </div>
         </Form>
     )
 }
--- src/components/item-card/ItemCard.styled.js
+++ src/components/item-card/ItemCard.styled.js
@@ -3,13 +3,11 @@ import styled from "styled-components";
 
 const size = {
     mobile:'425px',
-    laptop: '1024px',
-    desktop: '2560px'
+    desktop: '1024px',
 }
 
 export const device = {
     mobile: `(min-width: ${size.mobile})`,
-    laptop: `(min-width: ${size.laptop})`,
     desktop: `(min-width: ${size.desktop})`,
 };
 
@@ -17,64 +15,37 @@ export const Form = styled.div`
     display: flex;
     justify-content: center;
     width: 100%;
-`;
+    font-family: "Poppins", sans-serif;
 
-export const Card = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    border: 2px solid gray;
-    margin-bottom: 6px;
-    padding: 5px
-
-`;
-
-export const Info = styled.div`
-    @media ${device.iphone} {
+    .card {
         display: flex;
         justify-content: space-between;
-        flex-direction: column;
-        padding-left: 10px;
-        padding-right: 10px;
+        align-items: center;
+        width: 100%;
+        border: 2px solid gray;
+        margin-bottom: 6px;
+        padding: 5px    
     }
 
-    @media ${device.mobile} {
+    .info {
         display: flex;
         justify-content: space-between;
         flex-direction: column;
         padding-left: 10px;
         padding-right: 40px;
     }
-`;
 
-export const Title = styled.h3`
-    font-family: "Prompt", sans-serif;
-    font-weight: 700;
-    font-style: normal;
-`;
-
-export const Quantity = styled.h5`
-    font-family: "Prompt", sans-serif;
-    font-weight: 700;
-    font-style: normal;
-`;
-
-export const RemoveButton = styled.button`
-    @media ${device.iPhone} {
-        width: 180px;
-        height: 40px;
-        border-radius: 8px;
-        font-family: "Prompt", sans-serif;
+    .title {
         font-weight: 700;
-        font-style: normal;    
-        color: rgba(247, 247, 247, 1);
-        background-color: rgb(180, 180, 180);
-        border-color: rgba(63, 63, 63, 1)
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        font-style: normal;
     }
 
-    @media ${device.mobile} {
+    .quantity {
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    .remove-button {
         width: 170px;
         height: 40px;
         border-radius: 8px;
