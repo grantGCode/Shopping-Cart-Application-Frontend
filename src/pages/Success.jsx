71090fd7ded97645a504b5493fea4b07d2804a79
Grant Bentley <gbentley0618@comcast.net>
2024-10-01 16:17:40 -0500

    create Success.styled.js for Success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { SuccessStyledContainer} from '../styles/Success.styled.js';
+
 
 function success() {
   return (
-    <div>
-      <h1>success.js</h1>
-    </div>
+    <SuccessStyledContainer>
+      <h2 className='header-two'>You're <span className='red'>All</span></h2> <h2 className='header-two'>Set!</h2>
+      <h2 className='header-two'><i className='skinny'>Thank You!</i>.</h2>
+      {/* <p className='header-description'>{loremText}</p> */}
+      <button className='cta-button'>Return To Store</button>
+      <div className="store-container">
+        <div className='bottom-image-container'>
+          <img alt='girl-alt-logo' src='./images/aiony-haust-cutout-mobile.png'></img>
+        </div> 
+      </div>
+    </SuccessStyledContainer>
   )
 }
 
