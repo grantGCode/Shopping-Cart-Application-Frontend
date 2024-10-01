b2b96fe009c0984463498f240312a9df4cb976eb
Grant Bentley <gbentley0618@comcast.net>
2024-10-01 16:31:32 -0500

    header text added to Success

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -6,7 +6,19 @@ function success() {
     <SuccessStyledContainer>
       <h2 className='header-two'>You're <span className='red'>All</span></h2> <h2 className='header-two'>Set!</h2>
       <h2 className='header-two'><i className='skinny'>Thank You!</i>.</h2>
-      {/* <p className='header-description'>{loremText}</p> */}
+      <p className='header-description'>
+        Please be so kind enough to let us know what 
+        you think once your new jacket arrives. 
+      </p>
+      <p className='header-description'>
+        You will get an email from our team 
+        <span className='red'> two weeks </span> 
+        after your order has been delivered. 
+      </p>
+      <p className='header-description'>
+        We Appreciate Your Feedback!
+      </p>
+      <p></p>
       <button className='cta-button'>Return To Store</button>
       <div className="store-container">
         <div className='bottom-image-container'>
